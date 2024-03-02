24fdd205b5c1c2e1a9a5ebffa2aff2665ea149d4
Лезовский Станислав <lezsv@reso.ru>
2024-03-02 23:30:00 +0300

    Initial

---
+++ Sources/LoggingLib/LoggingLib.swift
@@ -0,0 +1,36 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import OSLog
+
+public extension Logger {
+    
+    private static var subsystem = Bundle.main.bundleIdentifier!
+    
+    static let viewCycle = Logger(subsystem: subsystem, category: "ViewcycleLogging")
+    static let statistics = Logger(subsystem: subsystem, category: "StatisticsLogging")
+    static let ui = Logger(subsystem: subsystem, category: "InterfaceLogging")
+    static let network = Logger(subsystem: subsystem, category: "NetworkLogging")
+    
+    func logWithDetails(level: OSLogType, message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        let log = OSLog(subsystem: Bundle.main.bundleIdentifier!, category: "CustomLogs")
+        let logMessage = "\(Date()): \(file.components(separatedBy: "/").last ?? "") - \(function) - Line \(line) - \(message)"
+        os_log("%{public}@", log: log, type: OSLogType.default, logMessage)
+    }
+
+    func logViewCycle(level: OSLogType, message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        logWithDetails(level: level, message: message, file: file, function: function, line: line)
+    }
+
+    func logStatistic(level: OSLogType, message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        logWithDetails(level: level, message: message, file: file, function: function, line: line)
+    }
+
+    func logUI(level: OSLogType, message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        logWithDetails(level: level, message: message, file: file, function: function, line: line)
+    }
+
+    func logNetwork(level: OSLogType, message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        logWithDetails(level: level, message: message, file: file, function: function, line: line)
+    }
+}

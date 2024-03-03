46bb0bf196c1425cdef0677a1ec8496c58b42cf8
Лезовский Станислав <lezsv@reso.ru>
2024-03-03 03:15:38 +0300

    Added Documentation

--- Sources/LoggingLib/LoggingLib.swift
+++ Sources/LoggingLib/LoggingLib.swift
@@ -1,32 +1,40 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+// MyAwesomePackage
+//
+// Ссылка на документацию или репозиторий
 
 import OSLog
 
 extension Logger {
+    /// Категории лог-файлов для различных типов логирования.
     public enum Categories: String {
+        /// - viewcycleLogging: Логирование событий жизненного цикла контроллеров или приложения.
         case viewcycleLogging = "viewcycleLogging"
+        /// - userInterfaceLogging: Логирование событий пользовательского интерфейса.
         case userInterfaceLogging = "userInterfaceLogging"
+        /// - networkLogging: Логирование событий сетевого взаимодействия.
         case networkLogging = "networkLogging"
+        /// - statisticsLogging: Общая категория логирования для неспецифичных событий и статистики.
         case statisticsLogging = "statisticsLogging"
     }
-        
+    /// Идентификатор бандла для поиска и фильтрации в Console.app..
     private static var subsystem = Bundle.main.bundleIdentifier!
+    /// Форматтер времени для лог файла.
     private static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm:ss"
         return formatter
     }()
-/// Log a message with the specified category, message, priority, and additional information.
-///
-/// - Parameters:
-///   - category: The category of the log.
-///   - message: The log message.
-///   - level: The priority level of the log.
-///   - file: The file path. By default, it is automatically filled with the calling file path.
-///   - function: The function name. By default, it is automatically filled with the calling function name.
-///   - line: The line number. By default, it is automatically filled with the calling line number.
-
+    /// Log a message with the specified category, message, priority, and additional information.
+    ///
+    /// - Parameters:
+    ///   - category: The category of the log.
+    ///   - message: The log message.
+    ///   - level: The priority level of the log.
+    ///   - file: The file path. By default, it is automatically filled with the calling file path.
+    ///   - function: The function name. By default, it is automatically filled with the calling function name.
+    ///   - line: The line number. By default, it is automatically filled with the calling line number.
     public static func log(
         for category: Categories,
         with message: String,
@@ -40,21 +48,23 @@ extension Logger {
         
         os_log("%{public}@", log: OSLog(subsystem: subsystem, category: category.rawValue), type: level, logMessage)
     }
-}
-
-//MARK: Icon managment
-
-private extension Logger {
-    static func iconForCategory(_ category: Categories) -> String {
+    /// Add icon to log depending on selected category.
+    ///
+    /// - Parameters:
+    ///   - category: The category of the log.
+    ///
+    /// - Returns:
+    ///  Returns icon according to log type.
+    internal static func iconForCategory(_ category: Categories) -> String {
         switch category {
-            case .viewcycleLogging:
-                return "🔄"
-            case .userInterfaceLogging:
-                return "⚙️"
-            case .networkLogging:
-                return "🌐"
-            case .statisticsLogging:
-                return "📊"
+        case .viewcycleLogging:
+            return "🔄"
+        case .userInterfaceLogging:
+            return "⚙️"
+        case .networkLogging:
+            return "🌐"
+        case .statisticsLogging:
+            return "📊"
         }
     }
 }

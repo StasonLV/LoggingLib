b4e99b290698bb70bc460593814cc9eb693e980a
Лезовский Станислав <lezsv@reso.ru>
2024-03-06 14:07:29 +0300

    Logging interface

--- Sources/LoggingPackage/LoggingPackage.swift
+++ Sources/LoggingPackage/LoggingPackage.swift
@@ -4,14 +4,26 @@
 
 import OSLog
 
-final public class Logging {
+protocol LoggingInterface {
+    static func log(
+        for category: Categories,
+        with message: String,
+        priority level: OSLogType,
+        file: String,
+        function: String,
+        line: Int,
+        appendToFile: Bool
+    )
+}
+
+final public class Logging: LoggingInterface {
     
     private static var subsystem: String! {
         didSet {
             makeLoggers()
         }
     }
-    static let sharedInstance: Logging = {
+    private static let sharedInstance: Logging = {
         let instance = Logging()
         subsystem = Bundle.main.bundleIdentifier ?? "undefinedSubsystemBundle"
         return instance
@@ -31,7 +43,8 @@ final public class Logging {
     deinit {
         print("Logger DEINITIALIZED")
     }
-    
+    ///  Создаем логгеры для каждой категории логгирования при первом обращении к функции log()(метод приватный).
+    ///
     private static func makeLoggers() {
         Logging.networkLogger = Logger(subsystem: subsystem, category: Categories.networkLogging.rawValue)
         Logging.viewcycleLogger = Logger(subsystem: subsystem, category: Categories.viewcycleLogging.rawValue)
@@ -40,7 +53,10 @@ final public class Logging {
         Logging.privateFileLogger = Logger(subsystem: subsystem, category: Categories.privateFileLogging.rawValue)
         Logging.socketLogger = Logger(subsystem: subsystem, category: Categories.socketLogging.rawValue)
     }
-    
+    ///  Получаем логгер в зависимости от переданной категории(метод приватный).
+    ///
+    /// - Parameters:
+    ///   - category: категория логгирования.
     private static func getLogger(for category: Categories) -> Logger {
         switch category {
         case .viewcycleLogging:
@@ -63,7 +79,7 @@ final public class Logging {
         formatter.dateFormat = "HH:mm:ss"
         return formatter
     }()
-    /// Log a message with the specified category, message, priority, and additional information.
+    ///  Log a message with the specified category, message, priority, and additional information.
     ///
     /// - Parameters:
     ///   - category: The category of the log.
@@ -82,7 +98,7 @@ final public class Logging {
         line: Int = #line,
         appendToFile: Bool = false
     ) {
-        let myInstance = sharedInstance
+        let _ = sharedInstance
         let logTime = dateFormatter.string(from: Date())
         let logMessage = "\(iconForCategory(category)) [\(logTime)][\(URL(fileURLWithPath: file).deletingPathExtension().lastPathComponent) on line: \(line)] - \(function)\n LOG MESSAGE:\n\(message)"
         
@@ -178,155 +194,156 @@ final public class Logging {
         }
     }
     
-    public enum Categories: String {
-        /// - viewcycleLogging: Логирование событий жизненного цикла контроллеров или приложения.
-        case viewcycleLogging = "viewcycleLogging"
-        /// - userInterfaceLogging: Логирование событий пользовательского интерфейса.
-        case userInterfaceLogging = "userInterfaceLogging"
-        /// - networkLogging: Логирование событий сетевого взаимодействия.
-        case networkLogging = "networkLogging"
-        /// - statisticsLogging: Общая категория логирования для неспецифичных событий и статистики.
-        case statisticsLogging = "statisticsLogging"
-        /// - privateFileLogging: Приватная категория для обработки записи/чтения логов в файл.
-        case privateFileLogging = "privateFileLogging"
-        /// - privateFileLogging: Приватная категория для обработки записи/чтения логов в файл.
-        case socketLogging = "socketLogging"
-    }
+
 }
-
-extension Logger {
-    /// Категории лог-файлов для различных типов логирования.
-    public enum Categories: String {
-        /// - viewcycleLogging: Логирование событий жизненного цикла контроллеров или приложения.
-        case viewcycleLogging = "viewcycleLogging"
-        /// - userInterfaceLogging: Логирование событий пользовательского интерфейса.
-        case userInterfaceLogging = "userInterfaceLogging"
-        /// - networkLogging: Логирование событий сетевого взаимодействия.
-        case networkLogging = "networkLogging"
-        /// - statisticsLogging: Общая категория логирования для неспецифичных событий и статистики.
-        case statisticsLogging = "statisticsLogging"
-        /// - privateFileLogging: Приватная категория для обработки записи/чтения логов в файл.
-        case privateFileLogging = "privateFileLogging"
-    }
-    /// Идентификатор бандла для поиска и фильтрации в Console.app.
-    private static var subsystem = Bundle.main.bundleIdentifier!
-    /// Форматтер времени для лог файла.
-    private static let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm:ss"
-        return formatter
-    }()
-    /// Log a message with the specified category, message, priority, and additional information.
-    ///
-    /// - Parameters:
-    ///   - category: The category of the log.
-    ///   - message: The log message.
-    ///   - level: The priority level of the log.
-    ///   - file: The file path. By default, it is automatically filled with the calling file path.
-    ///   - function: The function name. By default, it is automatically filled with the calling function name.
-    ///   - line: The line number. By default, it is automatically filled with the calling line number.
-    ///   - appendToFile: Specify if you want to add it to log.txt file.
-    public static func log(
-        for category: Categories,
-        with message: String,
-        priority level: OSLogType,
-        file: String = #file,
-        function: String = #function,
-        line: Int = #line,
-        appendToFile: Bool = false
-    ) {
-        let logTime = dateFormatter.string(from: Date())
-        let logMessage = "\(iconForCategory(category)) [\(logTime)][\(URL(fileURLWithPath: file).deletingPathExtension().lastPathComponent) on line: \(line)] - \(function)\n LOG MESSAGE:\n\(message)"
-        os_log("%{public}@", log: OSLog(subsystem: subsystem, category: category.rawValue), type: level, logMessage)
-        if appendToFile {
-            writeLogToFile(log: logMessage)
-            checkFileContent()
-        }
-    }
-    /// Write log message into log.txt file with the specified category, message, priority, and additional information.
-    ///
-    /// - Parameters:
-    ///   - log: Content of the log file.
-    private static func writeLogToFile(log: String) {
-#if DEBUG
-        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            self.log(for: .privateFileLogging, with: "Log file (log.txt) not found", priority: .error)
-            return
-        }
-        
-        let logFileURL = documentsDirectory.appendingPathComponent("log.txt")
-        
-        do {
-            let textToWrite = "\(log)\n"
-            
-            if let fileHandle = FileHandle(forWritingAtPath: logFileURL.path) {
-                fileHandle.seekToEndOfFile()
-                fileHandle.write(textToWrite.data(using: .utf8)!)
-                fileHandle.closeFile()
-            } else {
-                try textToWrite.write(to: logFileURL, atomically: true, encoding: .utf8)
-            }
-            
-//            self.log(for: .privateFileLogging, with: "Log successfully appended to file", priority: .default)
-
-        } catch {
-            self.log(for: .privateFileLogging, with: "Failed to write log to file: \(error.localizedDescription)", priority: .error)
-        }
-#endif
-    }
-    /// Check contents of log.txt file.
-    ///
-    private static func checkFileContent() {
-        guard let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("log.txt") else {
-            log(for: .privateFileLogging, with: "Log file (log.txt) not found", priority: .error)
-            return
-        }
-        
-        do {
-            let fileContent = try String(contentsOf: fileURL, encoding: .utf8)
-            log(for: .privateFileLogging, with: "Log file content:\n\(fileContent)", priority: .default)
-
-        } catch {
-            log(for: .privateFileLogging, with: "Error reading log file content: \(error.localizedDescription)", priority: .error)
-        }
-    }
-    /// Clears log file (log.txt).
-    ///
-    public static func clearLogFile() {
-        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            log(for: .privateFileLogging, with: "Log file (log.txt) not found", priority: .error)
-            return
-        }
-        
-        let logFileURL = documentsDirectory.appendingPathComponent("log.txt")
-        
-        do {
-            try FileManager.default.removeItem(at: logFileURL)
-            log(for: .privateFileLogging, with: "Log file cleared successfully", priority: .default)
-        } catch {
-            log(for: .privateFileLogging, with: "Failed to clear log file: \(error.localizedDescription)", priority: .default)
-        }
-    }
-
-    /// Add icon to log depending on selected category.
-    ///
-    /// - Parameters:
-    ///   - category: The category of the log.
-    ///
-    /// - Returns:
-    ///  Returns icon according to log type.
-    internal static func iconForCategory(_ category: Categories) -> String {
-        switch category {
-        case .viewcycleLogging:
-            return "🔄"
-        case .userInterfaceLogging:
-            return "⚙️"
-        case .networkLogging:
-            return "🌐"
-        case .statisticsLogging:
-            return "📊"
-        case .privateFileLogging:
-            return "♿️"
-        }
-    }
+public enum Categories: String {
+    /// - viewcycleLogging: Логирование событий жизненного цикла контроллеров или приложения.
+    case viewcycleLogging = "viewcycleLogging"
+    /// - userInterfaceLogging: Логирование событий пользовательского интерфейса.
+    case userInterfaceLogging = "userInterfaceLogging"
+    /// - networkLogging: Логирование событий сетевого взаимодействия.
+    case networkLogging = "networkLogging"
+    /// - statisticsLogging: Общая категория логирования для неспецифичных событий и статистики.
+    case statisticsLogging = "statisticsLogging"
+    /// - privateFileLogging: Приватная категория для обработки записи/чтения логов в файл.
+    case privateFileLogging = "privateFileLogging"
+    /// - privateFileLogging: Приватная категория для обработки записи/чтения логов в файл.
+    case socketLogging = "socketLogging"
 }
+//
+//extension Logger {
+//    /// Категории лог-файлов для различных типов логирования.
+//    public enum Categories: String {
+//        /// - viewcycleLogging: Логирование событий жизненного цикла контроллеров или приложения.
+//        case viewcycleLogging = "viewcycleLogging"
+//        /// - userInterfaceLogging: Логирование событий пользовательского интерфейса.
+//        case userInterfaceLogging = "userInterfaceLogging"
+//        /// - networkLogging: Логирование событий сетевого взаимодействия.
+//        case networkLogging = "networkLogging"
+//        /// - statisticsLogging: Общая категория логирования для неспецифичных событий и статистики.
+//        case statisticsLogging = "statisticsLogging"
+//        /// - privateFileLogging: Приватная категория для обработки записи/чтения логов в файл.
+//        case privateFileLogging = "privateFileLogging"
+//    }
+//    /// Идентификатор бандла для поиска и фильтрации в Console.app.
+//    private static var subsystem = Bundle.main.bundleIdentifier!
+//    /// Форматтер времени для лог файла.
+//    private static let dateFormatter: DateFormatter = {
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "HH:mm:ss"
+//        return formatter
+//    }()
+//    /// Log a message with the specified category, message, priority, and additional information.
+//    ///
+//    /// - Parameters:
+//    ///   - category: The category of the log.
+//    ///   - message: The log message.
+//    ///   - level: The priority level of the log.
+//    ///   - file: The file path. By default, it is automatically filled with the calling file path.
+//    ///   - function: The function name. By default, it is automatically filled with the calling function name.
+//    ///   - line: The line number. By default, it is automatically filled with the calling line number.
+//    ///   - appendToFile: Specify if you want to add it to log.txt file.
+//    public static func log(
+//        for category: Categories,
+//        with message: String,
+//        priority level: OSLogType,
+//        file: String = #file,
+//        function: String = #function,
+//        line: Int = #line,
+//        appendToFile: Bool = false
+//    ) {
+//        let logTime = dateFormatter.string(from: Date())
+//        let logMessage = "\(iconForCategory(category)) [\(logTime)][\(URL(fileURLWithPath: file).deletingPathExtension().lastPathComponent) on line: \(line)] - \(function)\n LOG MESSAGE:\n\(message)"
+//        os_log("%{public}@", log: OSLog(subsystem: subsystem, category: category.rawValue), type: level, logMessage)
+//        if appendToFile {
+//            writeLogToFile(log: logMessage)
+//            checkFileContent()
+//        }
+//    }
+//    /// Write log message into log.txt file with the specified category, message, priority, and additional information.
+//    ///
+//    /// - Parameters:
+//    ///   - log: Content of the log file.
+//    private static func writeLogToFile(log: String) {
+//#if DEBUG
+//        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+//            self.log(for: .privateFileLogging, with: "Log file (log.txt) not found", priority: .error)
+//            return
+//        }
+//        
+//        let logFileURL = documentsDirectory.appendingPathComponent("log.txt")
+//        
+//        do {
+//            let textToWrite = "\(log)\n"
+//            
+//            if let fileHandle = FileHandle(forWritingAtPath: logFileURL.path) {
+//                fileHandle.seekToEndOfFile()
+//                fileHandle.write(textToWrite.data(using: .utf8)!)
+//                fileHandle.closeFile()
+//            } else {
+//                try textToWrite.write(to: logFileURL, atomically: true, encoding: .utf8)
+//            }
+//            
+////            self.log(for: .privateFileLogging, with: "Log successfully appended to file", priority: .default)
+//
+//        } catch {
+//            self.log(for: .privateFileLogging, with: "Failed to write log to file: \(error.localizedDescription)", priority: .error)
+//        }
+//#endif
+//    }
+//    /// Check contents of log.txt file.
+//    ///
+//    private static func checkFileContent() {
+//        guard let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("log.txt") else {
+//            log(for: .privateFileLogging, with: "Log file (log.txt) not found", priority: .error)
+//            return
+//        }
+//        
+//        do {
+//            let fileContent = try String(contentsOf: fileURL, encoding: .utf8)
+//            log(for: .privateFileLogging, with: "Log file content:\n\(fileContent)", priority: .default)
+//
+//        } catch {
+//            log(for: .privateFileLogging, with: "Error reading log file content: \(error.localizedDescription)", priority: .error)
+//        }
+//    }
+//    /// Clears log file (log.txt).
+//    ///
+//    public static func clearLogFile() {
+//        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+//            log(for: .privateFileLogging, with: "Log file (log.txt) not found", priority: .error)
+//            return
+//        }
+//        
+//        let logFileURL = documentsDirectory.appendingPathComponent("log.txt")
+//        
+//        do {
+//            try FileManager.default.removeItem(at: logFileURL)
+//            log(for: .privateFileLogging, with: "Log file cleared successfully", priority: .default)
+//        } catch {
+//            log(for: .privateFileLogging, with: "Failed to clear log file: \(error.localizedDescription)", priority: .default)
+//        }
+//    }
+//
+//    /// Add icon to log depending on selected category.
+//    ///
+//    /// - Parameters:
+//    ///   - category: The category of the log.
+//    ///
+//    /// - Returns:
+//    ///  Returns icon according to log type.
+//    internal static func iconForCategory(_ category: Categories) -> String {
+//        switch category {
+//        case .viewcycleLogging:
+//            return "🔄"
+//        case .userInterfaceLogging:
+//            return "⚙️"
+//        case .networkLogging:
+//            return "🌐"
+//        case .statisticsLogging:
+//            return "📊"
+//        case .privateFileLogging:
+//            return "♿️"
+//        }
+//    }
+//}
--- Tests/LoggingPackageTests/LoggingPackageTests.swift
+++ Tests/LoggingPackageTests/LoggingPackageTests.swift
@@ -3,55 +3,55 @@ import OSLog
 @testable import LoggingPackage
 
 final class LoggingPackageTests: XCTestCase {
-    func testLogViewcycleLogging() {
-         let message = "View cycle started"
-         Logger.log(for: .viewcycleLogging, with: message, priority: .info)
-         
-         let expectation = XCTestExpectation(description: "Log Viewcycle Logging")
-         
-         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-             expectation.fulfill()
-         }
-         
-         wait(for: [expectation], timeout: 2)
-     }
-     
-     func testLogUserInterfaceLogging() {
-         let message = "User interface updated"
-         Logger.log(for: .userInterfaceLogging, with: message, priority: .debug)
-         
-         let expectation = XCTestExpectation(description: "Log UserInterface Logging")
-         
-         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-             expectation.fulfill()
-         }
-         
-         wait(for: [expectation], timeout: 2)
-     }
-     
-     func testLogNetworkLogging() {
-         let message = "Network request completed"
-         Logger.log(for: .networkLogging, with: message, priority: .debug)
-         
-         let expectation = XCTestExpectation(description: "Log Network Logging")
-         
-         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-             expectation.fulfill()
-         }
-         
-         wait(for: [expectation], timeout: 2)
-     }
-     
-     func testLogStatisticsLogging() {
-         let message = "Statistics collected"
-         Logger.log(for: .statisticsLogging, with: message, priority: .info)
-         
-         let expectation = XCTestExpectation(description: "Log Statistics Logging")
-         
-         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-             expectation.fulfill()
-         }
-         
-         wait(for: [expectation], timeout: 2)
-     }
+//    func testLogViewcycleLogging() {
+//         let message = "View cycle started"
+//         Logger.log(for: .viewcycleLogging, with: message, priority: .info)
+//         
+//         let expectation = XCTestExpectation(description: "Log Viewcycle Logging")
+//         
+//         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//             expectation.fulfill()
+//         }
+//         
+//         wait(for: [expectation], timeout: 2)
+//     }
+//     
+//     func testLogUserInterfaceLogging() {
+//         let message = "User interface updated"
+//         Logger.log(for: .userInterfaceLogging, with: message, priority: .debug)
+//         
+//         let expectation = XCTestExpectation(description: "Log UserInterface Logging")
+//         
+//         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//             expectation.fulfill()
+//         }
+//         
+//         wait(for: [expectation], timeout: 2)
+//     }
+//     
+//     func testLogNetworkLogging() {
+//         let message = "Network request completed"
+//         Logger.log(for: .networkLogging, with: message, priority: .debug)
+//         
+//         let expectation = XCTestExpectation(description: "Log Network Logging")
+//         
+//         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//             expectation.fulfill()
+//         }
+//         
+//         wait(for: [expectation], timeout: 2)
+//     }
+//     
+//     func testLogStatisticsLogging() {
+//         let message = "Statistics collected"
+//         Logger.log(for: .statisticsLogging, with: message, priority: .info)
+//         
+//         let expectation = XCTestExpectation(description: "Log Statistics Logging")
+//         
+//         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//             expectation.fulfill()
+//         }
+//         
+//         wait(for: [expectation], timeout: 2)
+//     }
  }

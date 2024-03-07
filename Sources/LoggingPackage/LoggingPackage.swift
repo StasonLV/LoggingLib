180e0652cddc88116a2dd8da4964b21c84bcc58a
Лезовский Станислав <lezsv@reso.ru>
2024-03-07 08:38:00 +0300

    Alerting

--- Sources/LoggingPackage/LoggingPackage.swift
+++ Sources/LoggingPackage/LoggingPackage.swift
@@ -16,7 +16,6 @@ protocol LoggingInterface {
         line: Int,
         appendToFile: Bool
     )
-    
     static func showModalView()
 }
 struct ListBackgroundModifier: ViewModifier {
@@ -65,6 +64,13 @@ struct LogView: View {
 
                             Button(action: {
                                 UIPasteboard.general.string = logText
+                                let alertController = UIAlertController(title: "Текст лога скопирован", message: nil, preferredStyle: .alert)
+                                        UIApplication.shared.windows.first?.rootViewController?.present(alertController, animated: true, completion: nil)
+
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                            alertController.dismiss(animated: true, completion: nil)
+                                        }
+
                             }) {
                                 Image(systemName: "doc.on.doc")
                             }
